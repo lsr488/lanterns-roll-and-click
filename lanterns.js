36e98ddd6d50a6e8e16b8041480335f5c7c87ffc
Lisa <lsr488@gmail.com>
2019-08-12 11:02:24 -0400

    Keep track of used experience circles.

--- lanterns.js
+++ lanterns.js
@@ -6,16 +6,47 @@ const completedPaths = [...pathCircles];
 const completedExp = [...expCircles];
 const assignedAbilities = [...abilities];
 
+console.log("expCircles:", expCircles);
+
+function countUsedExperienceCircles() {
+	let count = 0;
+	// let expCirclesAttributes = expCircles[1].attributes;
+	for(let i = 1; i < expCircles.length; i++) {
+		// console.log(expCircles[i].attributes); // DELETE ME
+		for(let j = 0; j < expCircles[i].attributes.length; j++) {
+			if(expCircles[i].attributes[j].name == "completed") {
+				// console.log("this is the completed attribute"); // DELETE ME
+				// console.log(expCircles[i].attributes[j].value); // DELETE ME
+				if(expCircles[i].attributes[j].value === "true") {
+					count++;					
+				}		
+			} 
+		}	
+	}
+	if(count === 5) {
+		console.log("First exp row completed.");
+	}
+	if(count === 9) {
+		console.log("Second exp row completed.");
+	}
+	if(count === 12) {
+		console.log("Third exp row completed.");
+	}	
+	console.log(count);
+}
+countUsedExperienceCircles();
+
 assignedAbilities.forEach(function(item) {
 	item.addEventListener("click", function(e) {
 		parentElement = e.target.parentNode;
 
 		if(parentElement.getAttribute("set") === "false") {
-			let input = prompt("How many circles? Enter a number 1 through 6.", "2");
+			const input = prompt("How many circles? Enter a number 1 through 6.");
 			if(input > 0 && input < 7) {
 				for(let i = 0; i < input; i++) {
 					const circle = document.createElement("i");
 					setAbility(circle);
+				
 					circle.addEventListener("click", function() {
 						useAbility(circle);
 					});
@@ -24,6 +55,7 @@ assignedAbilities.forEach(function(item) {
 			}	else {
 				alert("Choose a number 1 through 6.");
 			}	
+
 			parentElement.setAttribute("set", "true");
 		}
 	});
@@ -31,17 +63,17 @@ assignedAbilities.forEach(function(item) {
 
 completedPaths.forEach(function(item) {
 	item.addEventListener("click", function(e) {
-	if(item.getAttribute("completed") === "false") {
-		completePath(item);
-	}
+		if(item.getAttribute("completed") === "false") {
+			completePath(item);
+		}
 	});
 });
 
 completedExp.forEach(function(item) {
 	item.addEventListener("click", function(e) {
-	if(item.getAttribute("completed") === "false") {
-		useAbility(item);
-	}
+		if(item.getAttribute("completed") === "false") {
+			useAbility(item);
+		}
 	});
 });
 
