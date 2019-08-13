4ebbd3a11d1f6b0454b6b296b2175ba6da1f0a6d
Lisa <lsr488@gmail.com>
2019-08-12 22:09:31 -0400

    Reset buttons and dice displays added.

--- dice.js
+++ dice.js
@@ -1,7 +1,24 @@
 // need to figure out how to combine choosing a die and an ability
+const buttonRollAllDice = document.getElementById("roll-dice");
+const buttonResetRolledDice = document.getElementById("reset-rolls");
+const buttonResetBoth = document.getElementById("reset-both");
+const keptDiceDisplay = document.getElementById("kept-dice");
+const rolledDiceDisplay = document.getElementById("rolled-dice");
 
 let keptDice = [];
 let rolls = [];
+
+buttonRollAllDice.addEventListener("click", function() {
+	rollDice();
+});
+
+buttonResetRolledDice.addEventListener("click", function() {
+	resetRolls();	
+});
+
+buttonResetBoth.addEventListener("click", function() {
+	resetBoth();
+});
 
 function rollDice(numOfDice = "6") {
 	for(let i = 0; i < numOfDice; i ++) {
@@ -25,12 +42,43 @@ function chooseDice(...num) {
 
 function displayRolls() {
 	rolls.sort();
-	console.log("Rolls:", rolls);
+	rolledDiceDisplay.innerHTML = "Rolled Dice: <br/>" + changeDiceNumbersToIcons(rolls, rolledDiceDisplay);
 }
 
 function displayKeptDice() {
 	keptDice.sort();
-	console.log("Kept Dice:", keptDice);
+	keptDiceDisplay.innerHTML = "Kept Dice: <br/>" + changeDiceNumbersToIcons(keptDice, keptDiceDisplay);
+}
+
+function createDieIcon(diceArray, diceDisplay) {
+	const dieIcon = document.createElement("i");
+	dieIcon.innerHTML = changeDiceNumbersToIcons(diceArray);
+	diceDisplay.append(dieIcon);
+}
+
+function changeDiceNumbersToIcons(diceArray) {
+	let icon = "";
+	for(let i = 0; i < diceArray.length; i++) {
+		if(diceArray[i] === 1) {
+			icon += '<i class="fas fa-dice-one large"></i>';
+		}
+		if(diceArray[i] === 2) {
+			icon += '<i class="fas fa-dice-two large"></i>';
+		}
+		if(diceArray[i] === 3) {
+			icon += '<i class="fas fa-dice-three large"></i>';
+		}
+		if(diceArray[i] === 4) {
+			icon += '<i class="fas fa-dice-four large"></i>';
+		}
+		if(diceArray[i] === 5) {
+			icon += '<i class="fas fa-dice-five large"></i>';
+		}
+		if(diceArray[i] === 6) {
+			icon += '<i class="fas fa-dice-six large"></i>';
+		}
+	}
+	return icon;
 }
 
 function resetRolls() {
